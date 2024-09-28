2023be5869997b82e33cc5bddb754bfa39cf7c68
223880 <rafael.segala45@gmail.com>
2024-09-28 06:38:39 -0300

    Fix

--- src/utils.rs
+++ src/utils.rs
@@ -13,11 +13,13 @@ pub fn generate_nonce() -> Vec<u8> {
 
 // Function to serialize an object to JSON
 pub fn serialize_to_json<T: Serialize>(obj: &T) -> Result<String, serde_json::Error> {
-    serde_json::to_string(obj)
-}// Function to deserialize JSON data into a generic type
+    serde_json::to_string(obj);
+
+}
+// Function to deserialize JSON data into a generic type
 pub fn deserialize_from_json<T: for<'de> Deserialize<'de>>(json_data: &str) -> Result<T, serde_json::Error> {
-    serde_json::from_str(json_data)
-}// Function to generate a random nonce
+    serde_json::from_str(json_data);
+}
 // Function to generate a random nonce (renamed to avoid duplicate definition)
 pub fn generate_random_nonce() -> Vec<u8> {
     let mut rng = rand::thread_rng();
