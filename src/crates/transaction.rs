889032c032c0afe0a0d5676e2cba953fc7dd7a23
223880 <rafael.segala45@gmail.com>
2024-10-04 06:47:17 -0300

    Add network support and more

--- src/crates/transaction.rs
+++ src/crates/transaction.rs
@@ -2,6 +2,8 @@
 
 use crate::Transaction;
 use bitcoin::address::Address;
+use bitcoin::Network;
+use bitcoin::network::{Testnet, Signet, Mainnet};
 
 pub (crate) fn  transaction() -> Transaction {
     // Create a new transaction
@@ -12,6 +14,7 @@ pub fn create_coinjoin_transaction(
     _sender_address: &Address,
     _recipient_address: &str,
     _amount: u64,
+    _network: Network,
     _zk_proof: &super::proofs::ZKProof,
 ) -> bitcoin::Transaction {
     // Build a Bitcoin transaction here (you'll need to craft inputs, outputs)
